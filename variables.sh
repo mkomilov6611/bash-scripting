d5e12d1bcd11862ee407a1a9f2f31e26e0f9c081
Mukhammadnazar Komilov <mkomilov6611@gmail.com>
2022-08-08 11:48:36 +0500

    assign command output to a variable

--- variables.sh
+++ variables.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 MY_SHELL="bash"
+SERVER_NAME=$(hostname)
 
 echo "I like my $MY_SHELL shell"
 echo "I am ${MY_SHELL}ing on my laptop" 
+
+echo "You are running this script on ${SERVER_NAME}"
