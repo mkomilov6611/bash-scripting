ce0dd12ec7cce3c9ca9c19ecd9432fc895b55afb
Mukhammadnazar Komilov <mkomilov6611@gmail.com>
2022-08-09 13:06:16 +0500

    more example

--- 09_functions.sh
+++ 09_functions.sh
@@ -4,7 +4,7 @@ function sayHello() {
     # LOCAL var, if you exclude it will become GLOBAL after the function execution :)
     local GREETING="HELLO" 
     echo $GREETING
-    
+
     showCurrentTime
 }
 
@@ -13,3 +13,19 @@ function showCurrentTime() {
 }
 
 sayHello
+
+
+# another example
+function backUpFile() {
+    if [ -f $1 ]
+    then
+        local BACK="/tmp/$(basename ${1}).$(date +%F).$$"
+        
+        echo "Backing up $1 to  $BACK"
+        cp $1 $BACK
+    else
+        echo "File does not exist"
+        return 1
+    fi
+}
+backUpFile /etc/hosts && echo "Backup succeeded"
