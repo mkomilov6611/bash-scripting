a3792a7d517601190b85bb3a293e0244433bfd02
Mukhammadnazar Komilov <mkomilov6611@gmail.com>
2022-08-10 09:37:48 +0500

    ? example

--- 10_wildcards.sh
+++ 10_wildcards.sh
@@ -5,3 +5,10 @@ do
     echo "Copying $FILE"
     cp $FILE /var/www-just-html
 done
+
+# MORE ON WILDCARDS
+
+# *.txt (zero or more matches) -> any file that ends with txt
+
+# ?.txt (exactly one character match) -> any file with one char and txt ending
+
