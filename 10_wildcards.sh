206b72b9fe0b98c8b425c8ad75e927d4a4bc0f41
Mukhammadnazar Komilov <mkomilov6611@gmail.com>
2022-08-10 09:38:30 +0500

    built-in ranges

--- 10_wildcards.sh
+++ 10_wildcards.sh
@@ -16,3 +16,7 @@ done
 
 # [!0-9]?.txt -> any file that does NOT start with number and exactly one char long, and of course txt ending
 
+# [[:digit:]] = [0-9], 
+# [[:alpha:]] = [a-zA-Z], 
+# [[:alnum:]] = [a-zA-Z0-9], 
+# [[:space:]]  -> built-in ranges
