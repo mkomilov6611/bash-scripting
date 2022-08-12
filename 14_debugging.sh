3991a0714b48cc8ed04e91ae9800fcedb32c0045
Mukhammadnazar Komilov <mkomilov6611@gmail.com>
2022-08-12 16:05:22 +0500

    debugging commands, not complete

--- 14_debugging.sh
+++ 14_debugging.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -x
-# after some script
+# set -x Prints commands and their arguments as they executed
+# set -v Prints shell input lines as they read
+# set -e Exit immediately if a command exits with a non-zero status
 
-set +x # to stop debuggin
+# more debugging commands can be found here -> `help set`
